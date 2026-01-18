3cfecc5aa1bc63dd485791cee5c9ffd8ce79dc04
Nadya Lee <nadyalee1985@gmail.com>
2026-01-18 15:54:50 +0500

    insignificant update

--- src/test/java/com/epam/trainer_workload_service/cucumber/CucumberSpringConfiguration.java
+++ src/test/java/com/epam/trainer_workload_service/cucumber/CucumberSpringConfiguration.java
@@ -2,10 +2,12 @@ package com.epam.trainer_workload_service.cucumber;
 
 import com.epam.trainer_workload_service.dlq.TrainingWorkloadDlqProducer;
 import io.cucumber.spring.CucumberContextConfiguration;
+import jakarta.jms.ConnectionFactory;
 import org.springframework.boot.activemq.autoconfigure.ActiveMQAutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.jms.autoconfigure.JmsAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
@@ -22,10 +24,10 @@ public class CucumberSpringConfiguration {
     private TrainingWorkloadDlqProducer trainingWorkloadDlqProducer;
 
     @MockitoBean
-    private jakarta.jms.ConnectionFactory connectionFactory;
+    private ConnectionFactory connectionFactory;
 
     @MockitoBean
-    private org.springframework.jms.config.JmsListenerContainerFactory<?> jmsListenerContainerFactory;
+    private JmsListenerContainerFactory<?> jmsListenerContainerFactory;
 }
 
 
